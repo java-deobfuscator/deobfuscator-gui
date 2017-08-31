4f0c3f18682a29efd7e3cbe0c6d1414e311e7141
ThisTestUser <testsubjectforka1@gmail.com>
2017-08-31 16:31:22 -0400

    File selector

--- README.md
+++ README.md
@@ -3,14 +3,23 @@ An awesome GUI for an awesome deobfuscator
 
 ##What is Deobfuscator-GUI?
 Deobfuscator-GUI is a GUI for the deobfuscator located at https://github.com/java-deobfuscator/deobfuscator.
+
 It plans to make deobfuscating simple by having a UI, instead of having to deal with it from the command line.
+
 I currently use WindowBuilder to create the GUI: https://eclipse.org/windowbuilder/
 
 ##How to Use 
+1. Download the GUI from the releases page.
+
+2. Download the deobfuscator.jar from https://github.com/java-deobfuscator/deobfuscator.
 
 ##TODO
 []Input and output jar selection
+
 []Load and select transformers from deobfuscator jar (double text panels to fix order)
+
 []A list of libraries that can be loaded (manageable with add, remove) and adds rt.jar by default (probably going to use tabs to save space)
+
 []Input and output command equivalent (that can be run with deobfuscator.jar)
+
 []A console that can be used to check errors
--- src/io/github/thistestuser/DeobfuscatorFrame.java
+++ src/io/github/thistestuser/DeobfuscatorFrame.java
@@ -2,21 +2,21 @@ package io.github.thistestuser;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 import javax.swing.border.TitledBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.Font;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public class DeobfuscatorFrame
 {
 	private JFrame frame;
 	private static final String VERSION = "1.0-alpha";
 	private JTextField textField;
+	private File deobfuscatorPath;
 	
 	/**
 	 * Launch the application.
@@ -77,15 +77,42 @@ public class DeobfuscatorFrame
 		JButton selectDeob = new JButton("Select");
 		selectDeob.setToolTipText("<html>Here you will select the deobfuscator.jar you downloaded. <br>\r\nIf you don't have it, build the jar from <br>\r\nhttps://github.com/java-deobfuscator/deobfuscator</html>");
 		selectDeob.setBounds(451, 25, 89, 23);
+		selectDeob.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				JFileChooser deobFile = new JFileChooser();
+				deobFile.setFileSelectionMode(JFileChooser.FILES_ONLY);
+				deobFile.setAcceptAllFileFilterUsed(false);
+				deobFile.addChoosableFileFilter(new FileNameExtensionFilter(
+					"Executable JAR files", "jar"));
+				if(deobfuscatorPath != null)
+					deobFile.setSelectedFile(deobfuscatorPath);
+				int action = deobFile.showOpenDialog(null);
+				if(action == JFileChooser.APPROVE_OPTION)
+				{
+					deobfuscatorPath = deobFile.getSelectedFile();
+					String path = deobFile.getSelectedFile().toString();
+					textField.setText(path);
+					loadTransformers();
+				}
+			}
+		});
 		frame.getContentPane().add(selectDeob);
-		
-		JLabel lblDeobfuscatorJar = new JLabel("Deobfuscator Jar:");
-		lblDeobfuscatorJar.setBounds(22, 30, 110, 14);
-		frame.getContentPane().add(lblDeobfuscatorJar);
 		
 		textField = new JTextField();
 		textField.setBounds(117, 28, 315, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
+		
+		JLabel lblDeobfuscatorJar = new JLabel("Deobfuscator Jar:");
+		lblDeobfuscatorJar.setBounds(22, 30, 110, 14);
+		frame.getContentPane().add(lblDeobfuscatorJar);
+	}
+	
+	private void loadTransformers()
+	{
+		
 	}
 }
