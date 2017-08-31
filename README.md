baea9af342cdabbadf96e44f87a0cec4743d3350
ThisTestUser <testsubjectforka1@gmail.com>
2017-08-31 16:10:22 -0400

    First button

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # deobfuscator-gui
 An awesome GUI for an awesome deobfuscator
+
+##What is Deobfuscator-GUI?
+Deobfuscator-GUI is a GUI for the deobfuscator located at https://github.com/java-deobfuscator/deobfuscator.
+It plans to make deobfuscating simple by having a UI, instead of having to deal with it from the command line.
+I currently use WindowBuilder to create the GUI: https://eclipse.org/windowbuilder/
+
+##How to Use 
+
+##TODO
+[]Input and output jar selection
+[]Load and select transformers from deobfuscator jar (double text panels to fix order)
+[]A list of libraries that can be loaded (manageable with add, remove) and adds rt.jar by default (probably going to use tabs to save space)
+[]Input and output command equivalent (that can be run with deobfuscator.jar)
+[]A console that can be used to check errors
--- src/io/github/thistestuser/DeobfuscatorFrame.java
+++ src/io/github/thistestuser/DeobfuscatorFrame.java
@@ -3,11 +3,20 @@ package io.github.thistestuser;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.TitledBorder;
+
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class DeobfuscatorFrame
 {
 	private JFrame frame;
 	private static final String VERSION = "1.0-alpha";
+	private JTextField textField;
 	
 	/**
 	 * Launch the application.
@@ -44,8 +53,39 @@ public class DeobfuscatorFrame
 	private void initialize()
 	{
 		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 11));
 		frame.setBounds(100, 100, 580, 560);
-		frame.setTitle("Deobfuscator-GUI " + VERSION);
+		frame.setTitle("Deobfuscator-GUI " + VERSION + " By ThisTestUser");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}catch(ClassNotFoundException | InstantiationException
+			| IllegalAccessException | UnsupportedLookAndFeelException e)
+		{
+			e.printStackTrace();
+		}
+		
+		JLabel selectDeobfuscator = new JLabel("");
+		selectDeobfuscator.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		selectDeobfuscator.setBounds(10, 11, 544, 48);
+		TitledBorder selectTitle = new TitledBorder("Select Deobfuscator");
+		selectDeobfuscator.setBorder(selectTitle);
+		frame.getContentPane().add(selectDeobfuscator);
+		
+		JButton selectDeob = new JButton("Select");
+		selectDeob.setToolTipText("<html>Here you will select the deobfuscator.jar you downloaded. <br>\r\nIf you don't have it, build the jar from <br>\r\nhttps://github.com/java-deobfuscator/deobfuscator</html>");
+		selectDeob.setBounds(451, 25, 89, 23);
+		frame.getContentPane().add(selectDeob);
+		
+		JLabel lblDeobfuscatorJar = new JLabel("Deobfuscator Jar:");
+		lblDeobfuscatorJar.setBounds(22, 30, 110, 14);
+		frame.getContentPane().add(lblDeobfuscatorJar);
+		
+		textField = new JTextField();
+		textField.setBounds(117, 28, 315, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 	}
 }
