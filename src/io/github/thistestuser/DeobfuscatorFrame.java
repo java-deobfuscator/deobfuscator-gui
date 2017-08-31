ebf42b73c53968abd4c9a00d88114f0727da3b89
ThisTestUser <testsubjectforka1@gmail.com>
2017-08-31 15:19:20 -0400

    Rename frame

--- src/io/github/thistestuser/DeobfuscatorFrame.java
+++ src/io/github/thistestuser/DeobfuscatorFrame.java
@@ -4,9 +4,10 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
-public class MainFrame
+public class DeobfuscatorFrame
 {
 	private JFrame frame;
+	private static final String VERSION = "1.0-alpha";
 	
 	/**
 	 * Launch the application.
@@ -19,7 +20,7 @@ public class MainFrame
 			{
 				try
 				{
-					MainFrame window = new MainFrame();
+					DeobfuscatorFrame window = new DeobfuscatorFrame();
 					window.frame.setVisible(true);
 				}catch(Exception e)
 				{
@@ -32,7 +33,7 @@ public class MainFrame
 	/**
 	 * Create the application.
 	 */
-	public MainFrame()
+	public DeobfuscatorFrame()
 	{
 		initialize();
 	}
@@ -43,7 +44,8 @@ public class MainFrame
 	private void initialize()
 	{
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 580, 560);
+		frame.setTitle("Deobfuscator-GUI " + VERSION);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
