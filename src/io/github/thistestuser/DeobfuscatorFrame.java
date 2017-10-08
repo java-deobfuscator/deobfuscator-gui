2f4dbd59190f6e63fe3f463d29a3bc94149c7a55
ThisTestUser <testsubjectforka1@gmail.com>
2017-10-08 10:27:54 -0400

    Fix bug
    (2.0-BETA release contains this)

--- src/io/github/thistestuser/DeobfuscatorFrame.java
+++ src/io/github/thistestuser/DeobfuscatorFrame.java
@@ -549,12 +549,12 @@ public class DeobfuscatorFrame
 				System.setErr(print);
 				System.setOut(print);
 				// Runs it using reflection
-				thread = new Thread(new Runnable() 
+				if(DEOBFUSCATOR_VERSION == DeobfuscatorVersion.NEW)
 				{
-					@Override
-					public void run()
+					thread = new Thread(new Runnable() 
 					{
-						if(DEOBFUSCATOR_VERSION == DeobfuscatorVersion.NEW)
+						@Override
+						public void run()
 						{
 							try
 							{
@@ -594,100 +594,91 @@ public class DeobfuscatorFrame
 								loadClasses[0].getDeclaredMethod("start").invoke(deobfuscator);
 							}catch(Exception e)
 							{
-								e.printStackTrace(print);
+								e.printStackTrace();
 							}
-						}else
-						{
-							btnRun.setEnabled(false);
-							// Converts the above into args
-							List<String> command = new ArrayList<>();
-							command.add("java");
-							command.add("-jar");
-							command.add(deobfuscatorField.getText());
-							command.add("-input");
-							command.add(inputField.getText());
-							command.add("-output");
-							command.add(outputField.getText());
-							for(int i = 0; i < selectedTransformers.getSize(); i++)
-							{
-								command.add("-transformer");
-								command.add(selectedTransformers.get(i));
-							}
-							for(int i = 0; i < librariesList.getSize(); i++)
-							{
-								command.add("-path");
-								command.add(librariesList.get(i));
-							}
-							// Start
-							ProcessBuilder builder = new ProcessBuilder(command);
-							JFrame newFrame = new JFrame();
-							newFrame.setTitle("Console");
-							JTextArea area = new JTextArea();
-							area.setEditable(false);
-							newFrame.getContentPane().add(new JScrollPane(area));
-							newFrame.pack();
-							newFrame.setSize(800, 600);
-							newFrame.setVisible(true);
-							SwingWorker<Void, String> worker = new SwingWorker<Void, String>()
-							{
-								@Override
-								protected Void doInBackground() throws Exception
-								{
-									builder.redirectErrorStream(true);
-									Process process = builder.start();
-									DeobfuscatorFrame.this.process = process;
-									BufferedReader reader = new BufferedReader(
-										new InputStreamReader(process.getInputStream()));
-									String line;
-									while((line = reader.readLine()) != null)
-										publish(line);
-									return null;
-								}
-								
-								@Override
-								protected void process(List<String> chunks)
-								{
-									for(String line : chunks)
-									{
-										area.append(line);
-										area.append("\n");
-									}
-								}
-							};
-							worker.execute();
-							newFrame.addWindowListener(new WindowAdapter()
-					        {
-					            @Override
-					            public void windowClosing(WindowEvent e)
-					            {
-					            	btnRun.setEnabled(true);
-					            	worker.cancel(true);
-					            	if(process != null)
-					            	{
-					            		process.destroyForcibly();
-					            		process = null;
-					            	}
-					                e.getWindow().dispose();
-					            }
-					        });
 						}
+					});
+					newFrame.addWindowListener(new WindowAdapter()
+			        {
+			            @Override
+			            public void windowClosing(WindowEvent e)
+			            {
+			            	btnRun.setEnabled(true);
+			            	if(thread != null)
+			            	{
+			            		thread.stop();
+			            		thread = null;
+			            	}
+			                e.getWindow().dispose();
+			            }
+			        });
+				}else
+				{
+					btnRun.setEnabled(false);
+					// Converts the above into args
+					List<String> command = new ArrayList<>();
+					command.add("java");
+					command.add("-jar");
+					command.add(deobfuscatorField.getText());
+					command.add("-input");
+					command.add(inputField.getText());
+					command.add("-output");
+					command.add(outputField.getText());
+					for(int i = 0; i < selectedTransformers.getSize(); i++)
+					{
+						command.add("-transformer");
+						command.add(selectedTransformers.get(i));
 					}
-				});
-				thread.start();
-				newFrame.addWindowListener(new WindowAdapter()
-		        {
-		            @Override
-		            public void windowClosing(WindowEvent e)
-		            {
-		            	btnRun.setEnabled(true);
-		            	if(thread != null)
-		            	{
-		            		thread.destroy();
-		            		thread = null;
-		            	}
-		                e.getWindow().dispose();
-		            }
-		        });
+					for(int i = 0; i < librariesList.getSize(); i++)
+					{
+						command.add("-path");
+						command.add(librariesList.get(i));
+					}
+					// Start
+					ProcessBuilder builder = new ProcessBuilder(command);
+					SwingWorker<Void, String> worker = new SwingWorker<Void, String>()
+					{
+						@Override
+						protected Void doInBackground() throws Exception
+						{
+							builder.redirectErrorStream(true);
+							Process process = builder.start();
+							DeobfuscatorFrame.this.process = process;
+							BufferedReader reader = new BufferedReader(
+								new InputStreamReader(process.getInputStream()));
+							String line;
+							while((line = reader.readLine()) != null)
+								publish(line);
+							return null;
+						}
+						
+						@Override
+						protected void process(List<String> chunks)
+						{
+							for(String line : chunks)
+							{
+								area.append(line);
+								area.append("\n");
+							}
+						}
+					};
+					worker.execute();
+					newFrame.addWindowListener(new WindowAdapter()
+			        {
+			            @Override
+			            public void windowClosing(WindowEvent e)
+			            {
+			            	btnRun.setEnabled(true);
+			            	worker.cancel(true);
+			            	if(process != null)
+			            	{
+			            		process.destroyForcibly();
+			            		process = null;
+			            	}
+			                e.getWindow().dispose();
+			            }
+			        });
+				}
 			}
 		});
 	}
